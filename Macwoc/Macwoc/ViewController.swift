db1c13ce4ebf414f71a2f241d3299b426db3b8de
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-08-08 19:26:43 -0500

    Add everything

---
+++ Macwoc/Macwoc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Macwoc
+//
+//  Created by Sergio Daniel Lozano on 8/8/16.
+//  Copyright © 2016 zheref. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
