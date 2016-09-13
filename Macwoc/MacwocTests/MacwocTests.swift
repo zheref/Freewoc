ea25ba957f14f31430779fcc38d983d68575d8e5
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-09-13 16:23:31 -0500

    Convert to Swift 3

--- Macwoc/MacwocTests/MacwocTests.swift
+++ Macwoc/MacwocTests/MacwocTests.swift
@@ -28,7 +28,7 @@ class MacwocTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
