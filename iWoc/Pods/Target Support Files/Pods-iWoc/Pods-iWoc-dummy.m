e6619158aeb7aa217f99a6cb5183e539b6790f60
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-09-16 10:23:24 -0500

    Pod install for iWoc

---
+++ iWoc/Pods/Target Support Files/Pods-iWoc/Pods-iWoc-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iWoc : NSObject
+@end
+@implementation PodsDummy_Pods_iWoc
+@end
---
+++ iWoc/Pods/Target Support Files/Pods-iWocTests/Pods-iWocTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iWocTests : NSObject
+@end
+@implementation PodsDummy_Pods_iWocTests
+@end
